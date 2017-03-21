993d1f53de15fed9a972cebd8dfeb88ed4dc2950
marcoverzero <marcoverzero@gmail.com>
2017-03-21 17:10:45 -0600

    it works

--- credit_check.rb
+++ credit_check.rb
@@ -7,36 +7,40 @@ require 'pry'
 # form the check digit by subtracting the last digit of the sum from 10
 # (sum of the selectively doubled array + the check digit) modulo 10, if 0 valid, if not invalid
 class CreditChecker
-  attr_reader :card_string, :card_digits,:doubled, :undoubled
+  attr_reader :card_string,:doubled, :undoubled
   attr_accessor :presum, :check_digit, :sum, :check_number
   def initialize(card_string)
     @card_string = card_string
-    @card_digits = card_digits
-    @doubled = []
-    @undoubled = []
-    @presum = presum
-    @check_digit = check_digit
-    @sum = sum
-    @check_number = check_number
-    card_string_to_split_integers
   end
 
-  def card_string_to_split_integers
-    @card_digits = card_string.each_char.map(&:to_i)
+  def run
     selective_doubler
+    doubled_refiner
+    flatter_zipper
+    summer
+    checker_digit
+    validate
+  end
+
+  def card_digits
+    @card_digits ||= card_string.each_char.map(&:to_i)
   end
 
   def selective_doubler
+    @doubled = []
+    @undoubled = []
     card_digits.each_with_index do |digit,index|
           if index.odd?
             digit = digit * 2
-            doubled << digit
+            @doubled << digit
           else
-            undoubled << digit
+            @undoubled << digit
           end
         end
-        doubled_refiner
   end
+  # def cleaner_doubler
+  #   @doubled =[]
+  #   card_digits.
 
   def doubled_refiner
     doubled.map! do |digit|
@@ -46,43 +50,28 @@ class CreditChecker
         digit
       end
     end
-
-    flatter_zipper
   end
 
   def flatter_zipper
-    presum = undoubled.zip(doubled)
-    presum.flatten!
-    binding.pry
-
-    summer
-
+    @presum = undoubled.zip(doubled).flatten
   end
 
-#somehow at binding on :55 presum=>
-#[4, 9, 2, 9, 7, 6, 5, 8, 7, 5, 2, 1, 0, 1, 4, 6]
-#and at binding at :66 presum=>
-#nil
   def summer
-    binding.pry
-
-    sum = presum.reduce(:+)
-    checker_digit
+    self.sum = @presum.reduce(:+)
   end
 
   def checker_digit
-    check_number = (sum.to_s).split("")
-    check_digit = (10 - check_number[-1])
-    validate
+    check_number = sum % 10
+    self.check_digit = (10 - check_number)
   end
 
   def validate
     if (sum + check_digit) % 10 == 0
-      prints "The number is valid!"
+      puts "The number is valid!"
     else
-      prints "The number is invalid!"
+      puts "The number is invalid!"
     end
   end
 end
 
-CreditChecker.new("4929735477250543")
+CreditChecker.new("4929735477250543").run
